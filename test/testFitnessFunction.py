cd13b8f9724bcdb64f0d2bdb78ddbe68d9b35da0
Goldman <brian.goldman@mst.edu>
2012-05-12 23:34:40 -0600

    Naming convention in test

--- test/testFitnessFunction.py
+++ test/testFitnessFunction.py
@@ -7,7 +7,7 @@ class TestTrapFunctions(unittest.TestCase):
         self.config = {'trapSize': 5, 'stepSize': 2}
         self.traps = 20
 
-    def test_DeceptiveTrap(self):
+    def testDeceptiveTrap(self):
         box = FitnessFunction.DeceptiveTrap(self.config)
         self.assertEqual(box.evaluate([0, 0, 0, 0, 0] * self.traps), 4.0 / 5)
         self.assertEqual(box.evaluate([0, 1, 0, 0, 0] * self.traps), 3.0 / 5)
@@ -16,7 +16,7 @@ class TestTrapFunctions(unittest.TestCase):
         self.assertEqual(box.evaluate([1, 1, 1, 1, 0] * self.traps), 0.0 / 5)
         self.assertEqual(box.evaluate([1, 1, 1, 1, 1] * self.traps), 5.0 / 5)
 
-    def test_DeceptiveStepTrap(self):
+    def testDeceptiveStepTrap(self):
         box = FitnessFunction.DeceptiveStepTrap(self.config)
         self.assertEqual(box.evaluate([0, 0, 0, 0, 0] * self.traps), 2.0 / 3)
         self.assertEqual(box.evaluate([0, 1, 0, 0, 0] * self.traps), 2.0 / 3)
